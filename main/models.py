bc88abacfb67c7eacf2c29b0f89037718d7358ad
Poklok <vlad.narchyk@gmail.com>
2022-04-05 21:18:30 +0300

    add comments

--- main/models.py
+++ main/models.py
@@ -1,14 +1,31 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
+from django.db.models import ForeignKey
+
+
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
-    img = models.ImageField(upload_to='images/posts',)
+    img = models.ImageField(upload_to='images/posts', )
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Комментарий от {self.name} для {self.post}'
+
+    class Meta:
+        verbose_name = 'Комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-created',)
