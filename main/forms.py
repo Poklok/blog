44769274c37f8d0db8eb362c0c3d11d0cabc620d
Poklok <vlad.narchyk@gmail.com>
2022-04-23 18:44:19 +0300

    connect postgres

---
+++ main/forms.py
@@ -0,0 +1,9 @@
+from django.forms import ModelForm
+
+from main.models import Comment
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'text']
--- main/models.py
+++ main/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-from django.db.models import ForeignKey
 
 
 class Post(models.Model):
